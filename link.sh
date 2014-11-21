1718f6d5670c9972d353728702d3d38d3a621a7a
Masaru Watanabe <watanabe_masaru@cyberagent.co.jp>
2014-11-21 20:45:59 +0900

    修正

--- link.sh
+++ link.sh
@@ -2,36 +2,37 @@
 
 APP_DIR=$(cd $(dirname $0) && pwd)
 LINK_DIR=$(cd ${HOME} && pwd)
+LIST=$(cat list | grep -v "#")
 
 if [ "${LINK_DIR}" = "" ]; then
   echo "link directory is not found" 1>&2
   exit 1
 fi
 
-echo $LINK_DIR
-
-for dotfile in $(cat link_list); do
+for dotfile in ${LIST}; do
 
   LINK_FROM=${LINK_DIR}/${dotfile}
   LINK_TO=${APP_DIR}/${dotfile}
 
+  # 不正なファイル
   if [ ! -e ${LINK_TO} ]; then
     echo "${dotfile} is not found."
     continue
   fi
 
-  if [ ! -e ${LINK_FROM} ]; then
-    ln -s ${LINK_TO} ${LINK_FROM}
-    echo "${dotfile} is linked."
-    continue
-  fi
+  # ディレクトリの作成
+  mkdir -p $(dirname ${LINK_TO})
 
-  diff=$(diff ${LINK_FROM} ${LINK_TO})
-  if [ "${diff}" = "" ]; then
+  # リンクがない or 差分がないがない場合、リンクで上書き
+  if [ ! -e ${LINK_FROM} -o "$(diff ${LINK_FROM} ${LINK_TO})" = "" ]; then
     ln -fs ${LINK_TO} ${LINK_FROM}
     echo "${dotfile} is linked."
+    continue
+
+  # 差分があれば確認してリンク
   else
     ln -is ${LINK_TO} ${LINK_FROM}
   fi
+
 done
 
